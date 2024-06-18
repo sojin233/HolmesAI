d2f0f24764b42789a46d5425307a972e99a744ce
sojin <sojin233@naver.com>
2024-06-18 14:59:54 +0900

    list 추가

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // list box show hide
+  $(".list_box li").click(function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
+  $(".list_box li:first-child").click(function () {
+    $(".list_content_box .list01").addClass("show");
+    $(".list_content_box .list02").removeClass("show");
+  });
+  $(".list_box li:nth-child(2)").click(function () {
+    $(".list_content_box .list02").addClass("show");
+    $(".list_content_box .list01").removeClass("show");
+  });
+});
